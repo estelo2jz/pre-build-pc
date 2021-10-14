8c5b2c550bf4e4d0bdc0979137b9c30bbf894cbe
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 15:14:00 -0500

    Create Financing.js

---
+++ src/components/pages/Financing/Financing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Financing = () => {
+  return (
+    <div>
+      financing
+      {/* top header BG */}
+      {/* financing available */}
+      {/* rate component */}
+    </div>
+  )
+}
+
+export default Financing
