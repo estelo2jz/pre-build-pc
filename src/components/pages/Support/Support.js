7d251d751eef903195136da39ecb303cd763f093
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 15:14:05 -0500

    Create Support.js

---
+++ src/components/pages/Support/Support.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Support = () => {
+  return (
+    <div>
+      support
+      {/* top header bg */}
+      {/* how to guides component */}
+      {/* FAQ & AQ questions component */}
+      {/* support component */}
+      {/* contact our team component */}
+    </div>
+  )
+}
+
+export default Support
