dff62a4287afb854d9362e445dfdb577ae6460c7
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 15:13:56 -0500

    Create Home.js

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* top header with BG */}
+      {/* Pricing component */}
+      {/* review component */}
+      {/* best sellers component*/}
+      {/* follow component */}
+      {/* from  */}
+    </div>
+  )
+}
+
+export default Home
