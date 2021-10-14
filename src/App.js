d02fb28b59b674a8d4daef9bd9f8153e2c899e7b
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 14:52:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Products from './components/Products'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {DataProvider} from './components/DataProvider'
+import Details from './components/Details'
+import Cart from './components/Cart'
+
 
 function App() {
   return (
-    <div className="App">
-      Build REDUX
-    </div>
+    <DataProvider>
+      <div className="App">
+        <Router>
+          <Header />
+
+          <section>
+            <Routes>
+              <Route path="products" element={ <Products /> } />
+              <Route path="products/:id" element={ <Details /> } />
+              <Route path="cart" element={ <Cart /> } />
+            </Routes>
+          </section>
+        </Router>
+      </div>
+    </DataProvider>
   );
 }
 
