858ea1cc2f8ac1f81b46bde18829e0ed4e5e6e0a
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 15:14:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {components} from 'react';
 import Header from './components/Header'
 import Products from './components/Products'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {DataProvider} from './components/DataProvider'
 import Details from './components/Details'
 import Cart from './components/Cart'
+
+import About from './components/pages/AboutUs/About';
+import BuildPC from './components/pages/BuildPC/BuildPC';
+import Financing from './components/pages/Financing/Financing';
+import Review from './components/pages/Review/Review';
+import Support from './components/pages/Support/Support';
 
 
 function App() {
@@ -16,9 +22,15 @@ function App() {
 
           <section>
             <Routes>
-              <Route path="products" element={ <Products /> } />
-              <Route path="products/:id" element={ <Details /> } />
-              <Route path="cart" element={ <Cart /> } />
+              <Route path="products" exact element={ <Products /> } />
+              <Route path="products/:id" exact element={ <Details /> } />
+              <Route path="about" exact element={ <About /> } />
+              <Route path="build-your-pc" exact element={ <BuildPC /> } />
+              <Route path="financing" exact element={ <Financing /> } />
+              <Route path="review" exact element={ <Review /> } />
+              <Route path="support" exact element={ <Support /> } />
+
+              <Route path="cart" exact element={ <Cart /> } />
             </Routes>
           </section>
         </Router>
