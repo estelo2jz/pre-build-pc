af28f8152b32e4fbc239758b21e59f602fc67cd7
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-14 15:14:02 -0500

    Create Review.js

---
+++ src/components/pages/Review/Review.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Review = () => {
+  return (
+    <div>
+      review
+      {/* reviews top header */}
+      {/* reviews data */}
+    </div>
+  )
+}
+
+export default Review
