1b133c8dfdd78cdc5c2bbdb99f1450f8b5c31cfb
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-18 13:48:21 -0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import TopHeader from './TopHeader';
+import Pricing from './Pricing';
+import Review from './Review';
+import BestSellers from './BestSellers';
+import Follow from './Follow';
 
 const Home = () => {
   return (
     <div>
       {/* top header with BG */}
+      <TopHeader />
       {/* Pricing component */}
       {/* review component */}
       {/* best sellers component*/}
