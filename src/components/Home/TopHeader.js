b034a977b16f5ad52fcb49a3f6b7309414ef3dde
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-18 13:48:24 -0500

    Create TopHeader.js

---
+++ src/components/Home/TopHeader.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TopHeader = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>GAMING PCS</h1>
+          <p>Optimized for your budget.</p>
+        </div>
+        <div>
+          <div>
+            <button>START YOUR BUILD</button>
+          </div>
+          <div>
+            <button>BEST SELLERS</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopHeader;
