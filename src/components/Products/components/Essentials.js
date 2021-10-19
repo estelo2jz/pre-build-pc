143b5f7f806fe780fa5efb9d0441ad6ea06e140e
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-19 13:41:36 -0500

    Create Essentials.js

---
+++ src/components/Products/components/Essentials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/Essentials.scss';
+
+const Essentials = () => {
+  return (
+    <div className="essentials">
+      <div className="essentials__container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Essentials
