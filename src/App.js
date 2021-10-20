5340daa79ef18c667ccdf55ad2373360cbf753a9
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-20 14:18:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
-import React, {components} from 'react';
-import Header from './components/Header/Header'
+import React, { components } from "react";
+import Header from "./components/Header/Header";
 // import Products from './components/Products'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import {DataProvider} from './components/DataProvider'
 // import Details from './components/Details'
-import Cart from './components/Products/Cart'
+import Cart from "./components/Products/Cart";
+import ScrollToTop from "./components/ScrollToTop";
 
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
 
-import Products from './components/Products//Products';
-import {DataProvider} from './components/Products/Data/DataProvider';
-import Details from './components/Products/Details/Details';
+import Products from "./components/Products//Products";
+import { DataProvider } from "./components/Products/Data/DataProvider";
+import Details from "./components/Products/Details/Details";
 
-import About from './components/pages/AboutUs/About';
-import BuildPC from './components/pages/BuildPC/BuildPC';
-import Financing from './components/pages/Financing/Financing';
-import Review from './components/pages/Review/Review';
-import Support from './components/pages/Support/Support';
-import Footer from './components/Footer/Footer';
-
+import About from "./components/pages/AboutUs/About";
+import BuildPC from "./components/pages/BuildPC/BuildPC";
+import Financing from "./components/pages/Financing/Financing";
+import Review from "./components/pages/Review/Review";
+import Support from "./components/pages/Support/Support";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <DataProvider>
-      <div className="App">
-        <Router>
-          <Header />
+      <DataProvider>
+        <div className="App">
+          <Router>
+            <ScrollToTop>
+              <Header />
 
-          <section>
-            <Routes>
-              <Route path="/" exact element={ <Home /> } />
+              <section>
+                <Routes>
+                  <Route path="/" exact element={<Home />} />
 
-              <Route path="/products" exact element={ <BuildPC /> } />
-              <Route path="/pages/review" exact element={ <Review /> } />
-              <Route path="/pages/financing" exact element={ <Financing /> } />
-              <Route path="/pages/support" exact element={ <Support /> } />
-              <Route path="/pages/why-pre-build" exact element={ <About /> } />
+                  <Route path="/products" exact element={<BuildPC />} />
+                  <Route path="/pages/review" exact element={<Review />} />
+                  <Route path="/pages/financing" exact element={<Financing />} />
+                  <Route path="/pages/support" exact element={<Support />} />
+                  <Route path="/pages/why-pre-build" exact element={<About />} />
 
-              {/* <Route path="/products" exact element={ <Products /> } /> */}
-              <Route path="/products/:id" exact element={ <Details /> } />
+                  {/* <Route path="/products" exact element={ <Products /> } /> */}
+                  <Route path="/products/:id" exact element={<Details />} />
 
-              <Route path="cart" exact element={ <Cart /> } />
-            </Routes>
-          </section>
-          <Footer />
-        </Router>
-      </div>
-    </DataProvider>
+                  <Route path="cart" exact element={<Cart />} />
+                </Routes>
+              </section>
+              <Footer />
+            </ScrollToTop>
+          </Router>
+        </div>
+      </DataProvider>
   );
 }
 
