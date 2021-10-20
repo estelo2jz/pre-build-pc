40d1eca61f5bf873e551a1b415da233dc85d2873
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-20 14:18:06 -0500

    Update Review.js

--- src/components/pages/Review/Review.js
+++ src/components/pages/Review/Review.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ReviewComponent from '../../Home/Review';
 
 const Review = () => {
   return (
     <div>
-      review
+      <ReviewComponent />
       {/* reviews top header */}
       {/* reviews data */}
     </div>
