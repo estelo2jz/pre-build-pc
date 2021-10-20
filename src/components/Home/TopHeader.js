ff3622b7f11ddfad904f299f16d2f926c89305b5
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-20 14:17:56 -0500

    Update TopHeader.js

--- src/components/Home/TopHeader.js
+++ src/components/Home/TopHeader.js
@@ -1,18 +1,24 @@
 import React from "react";
 
+import "./styles/TopHeader.scss";
+
 const TopHeader = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h1>GAMING PCS</h1>
-          <p>Optimized for your budget.</p>
+    <div className="top-header">
+      <div className="top-header__container">
+        <div className="top-header__heading">
+          <div className="top-header__heading-top">
+            <p>GAMING PCS</p>
+          </div>
+          <div className="top-header__heading-bottom">
+            <p>Optimized for your budget.</p>
+          </div>
         </div>
-        <div>
-          <div>
+        <div className="top-header__bottom">
+          <div className="top-header__build">
             <button>START YOUR BUILD</button>
           </div>
-          <div>
+          <div className="top-header__best">
             <button>BEST SELLERS</button>
           </div>
         </div>
