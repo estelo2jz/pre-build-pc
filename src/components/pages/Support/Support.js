550fc6b638e4f439c16f818d19047d4f0198798d
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-25 14:01:45 -0500

    Update Support.js

--- src/components/pages/Support/Support.js
+++ src/components/pages/Support/Support.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
+
+import SupportTopHeader from "./SupportTopHeader";
+import SupportGuides from "./SupportGuides";
+import SupportQuestions from "./SupportQuestions";
+import SupportSales from "./SupportSales";
+import SupportSupport from "./SupportSupport";
+import SupportContact from "./SupportContact";
 
 const Support = () => {
   return (
     <div>
-      support
       {/* top header bg */}
+      <SupportTopHeader />
       {/* how to guides component */}
+      <SupportGuides />
       {/* FAQ & AQ questions component */}
-      {/* support component */}
+      <SupportQuestions />
       {/* contact our team component */}
+      <SupportContact />
     </div>
-  )
-}
+  );
+};
 
-export default Support
+export default Support;
