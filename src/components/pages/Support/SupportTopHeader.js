ba0bba2d49644f36bc19991e605befba639fea7e
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-25 14:01:55 -0500

    Create SupportTopHeader.js

---
+++ src/components/pages/Support/SupportTopHeader.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./styles/SupportTopHeader.scss";
+
+const SupportTopHeader = () => {
+  return (
+    <div className="support-top-header">
+      <div className="support-top-header__heading">
+        <p>SUPPORT CENTER.</p>
+      </div>
+      <div className="support-top-header__text">
+        <p>
+          We're here to help if you run into issues. Our team understands how
+          important it is for our customers to have their questions or issues
+          resolved quickly.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SupportTopHeader;
