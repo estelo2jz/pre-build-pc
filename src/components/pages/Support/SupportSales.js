5cd65d4064f6942227fe1563ff7d736a3d347ecb
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-25 14:01:38 -0500

    Create SupportSales.js

---
+++ src/components/pages/Support/SupportSales.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./styles/SupportSales.scss";
+
+const SupportSales = () => {
+  return (
+    <div className="support-sale">
+      <div className="support-sale__heading">
+        <p>SALES</p>
+      </div>
+      <div className="support-sale__text">
+        <p>
+          Buying a new PC may feel overwhelming, but we've made it simple and
+          easy. Here's a list of common questions and answers we come across
+          often. For additional help, contact our team for expert advice.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SupportSales;
