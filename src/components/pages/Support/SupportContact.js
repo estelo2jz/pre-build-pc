26ad87c9c9081c223c5d856943c4e7c2fbcd808b
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-25 14:01:47 -0500

    Create SupportContact.js

---
+++ src/components/pages/Support/SupportContact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "./styles/SupportContact.scss";
+
+const SupportContact = () => {
+  return (
+    <div className="support-contact">
+      <div className="support-contact__heading">
+        <p>CONTACT OUR TEAM.</p>
+      </div>
+      <div className="support-contact__text">
+        <p>Email: <span>support@buildredux.com</span></p>
+        <p>Phone: 1-856-390-7905</p>
+        <p>Hours: Mon-Fri: 9 AM - 5 PM (PST)</p>
+      </div>
+    </div>
+  )
+}
+
+export default SupportContact
