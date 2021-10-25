5eab3a7aedd8808c440239da08acca303496885a
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-25 14:01:44 -0500

    Create SupportGuides.js

---
+++ src/components/pages/Support/SupportGuides.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./styles/SupportGuides.scss";
+
+const SupportGuides = () => {
+  return (
+    <div className="support-guides">
+      <div className="support-guides__heading">
+        <p>HOW TO GUIDES.</p>
+      </div>
+      <div className="support-guides__text">
+        <p>
+          Check back soon, we're working on adding simple yet effective guides
+          on how to resolve a majority of all PC related issues. Until then, our
+          support team is ready to help.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SupportGuides;
