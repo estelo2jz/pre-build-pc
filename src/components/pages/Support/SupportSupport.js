8834a5f0f0e97956058d551f8eaebc54ba5638f1
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-25 14:01:48 -0500

    Create SupportSupport.js

---
+++ src/components/pages/Support/SupportSupport.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./styles/SupportSupport.scss";
+
+const SupportSupport = () => {
+  return (
+    <div className="support-support">
+      <div className="support-support__heading">
+        <p>SUPPORT</p>
+      </div>
+      <div className="support-support__text">
+        <p>
+          Good news, a majority of the issues you may face are quite simple and
+          easy for us to help resolve for you. Let's start with the common ones
+          below. If you still need help, please reach out to our team. Issues
+          while playin
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SupportSupport;
