3e01e2d8b05dfee93b4857efe09e8b254798bb4a
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-26 11:07:36 -0500

    Create SupportData.js

---
+++ src/components/pages/Support/Data/SupportData.js
@@ -0,0 +1,17 @@
+export const Data = [
+  {
+    question: "Issues while playing games?",
+    answer:
+      "Updating your graphics card driver and checking for any new patches for your game tends to resolve most issues.",
+  },
+  {
+    question: "No video on display when you turn on the PC?",
+    answer:
+      "Make sure your display is plugged into the graphics card instead of the motherboard. Verify the monitor is set to the correct input.",
+  },
+  {
+    question: "Blue screen or other stability issues?",
+    answer:
+      "If you just received your system, the graphics card or memory modules may need to be reseated. If the PC has been working fine in the past, try to do a Windows system point restore first, if that doesn't help use the Windows Memory Diagnostic test.",
+  },
+];
