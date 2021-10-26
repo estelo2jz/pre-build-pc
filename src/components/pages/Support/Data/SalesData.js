d535be291f6070bafd902a88ce0a1f2c96b8496c
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-26 11:07:25 -0500

    Create SalesData.js

---
+++ src/components/pages/Support/Data/SalesData.js
@@ -0,0 +1,22 @@
+export const Data = [
+  {
+    question: "How do I place an order?",
+    answer:
+      "Click 'Build Your PC' at the top of our website to get started. Our online PC builder will guide you step-by-step on building the right gaming PC for your needs and budget.",
+  },
+  {
+    question: "How long will it take?",
+    answer:
+      "Once we process your order, we should have it shipped within 3-4 week. You will receive tracking information via email.",
+  },
+  {
+    question: "What is your return policy?",
+    answer:
+      "We offer a 30 day return policy from the day you receive your order, re-stocking fees may apply unless there is an issue with the PC.",
+  },
+  {
+    question: "Do you setup and install Windows, drivers, etc?",
+    answer:
+      "Yes, our team will take care of everything for you. Your computer will be ready to play when you receive it. Our team will build, setup, and test the PC before it ships.",
+  },
+];
