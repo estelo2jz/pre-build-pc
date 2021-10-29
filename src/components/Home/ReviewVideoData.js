e38b10e94bf33a2ad2bae9214696a65ee4823671
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-10-29 12:04:30 -0500

    Create ReviewVideoData.js

---
+++ src/components/Home/ReviewVideoData.js
@@ -0,0 +1,68 @@
+
+import ReviewAvatar1 from "../../assets/images/review/mrtop.jpg";
+import ReviewAvatar2 from "../../assets/images/review/shortcircuit.jpg";
+import ReviewAvatar3 from "../../assets/images/review/dannylee.jpg";
+import ReviewAvatar4 from "../../assets/images/review/marc.jpg";
+import ReviewAvatar5 from "../../assets/images/review/juniper.jpg";
+import ReviewAvatar6 from "../../assets/images/review/rodey.jpg";
+import ReviewAvatar7 from "../../assets/images/review/frustrated.jpg";
+import ReviewAvatar8 from "../../assets/images/review/tech.jpg";
+
+export const Data = [
+  {
+    name: "Mr Top 5.",
+    comments: "Thanks to @MrTop5 for showing off the unboxing of the #BuildRedux PC! Check out his video to see his PC and setup!",
+    img: ReviewAvatar1,
+    video: "https://www.youtube.com/watch?v=4vpPJb392Vg",
+    summary: "This computer is absolutely insane! Once again if you want to check out this brand new Redux gaming PC - it's an absolute super computer. Highly, highly recommend it.",
+  },
+  {
+    name: "Short Circuit.",
+    comments: "Thanks for the awesome review @ShortCircuit!",
+    img: ReviewAvatar2,
+    video: "https://www.youtube.com/watch?v=UhO7MLntkDE",
+    summary: "You basically just pick which games you play, which performance you want, and it'll suggest a rig for you. They make things much, much simpler and again for only $75 bucks? Okay!",
+  },
+  {
+    name: "Danny Le.",
+    comments: "Thanks @nerdonabudget for your great review of the #BuildRedux PC!",
+    img: ReviewAvatar3,
+    video: "https://www.youtube.com/watch?v=BTMdT1pvJus",
+    summary: "To wrap it up I can without hesitation put my stamp of approval on Redux based on what I've seen. I think it's great to see another builder pop up in the industry and drive more competition within the space.",
+  },
+  {
+    name: "Marc Aranibar.",
+    comments: "Awesome @MarcAranibar! We're proud to have sold you on a prebuilt system!",
+    img: ReviewAvatar4,
+    video: "https://www.youtube.com/watch?v=Le8gwj0gZ7g",
+    summary: "I would take a look at Redux at the top of your list. They're one of the better manufacturers out there and on a part level, warranty and value level Redux PC's are a great deal for prebuilt systems.",
+  },
+  {
+    name: "Juniper Jd.",
+    comments: "The #BuildRedux PC will look great in that setup! @JuniperJD",
+    img: ReviewAvatar5,
+    video: "https://www.youtube.com/watch?v=uFUqZP11NaU",
+    summary: "It's crazy how well the PC is able to run and render things that would otherwise lag on most other devices! It looks great, it runs great, it's just phenomenal and I am very grateful to have it.",
+  },
+  {
+    name: "RodeyBros.",
+    comments: "First win with #BuildRedux! Check out @RodeyBros full unboxing on YouTube!",
+    img: ReviewAvatar6,
+    video: "https://www.youtube.com/watch?v=Y5FJnSN6kG0",
+    summary: "I did an entire stream on the Redux after recording this video and it felt great, I played great and over that entire five hours the PC held up just fine. I had steady frames the entire time which I can honestly say I haven't had in a long long time. It feels good and hopefully with this new PC we're gonna be placing in some tournaments!",
+  },
+  {
+    name: "The Frustrated Gamer.",
+    comments: "Looking good! Make sure to follow @frustrated_gamr on YouTube to see some amazing gameplay!",
+    img: ReviewAvatar7,
+    video: "https://www.youtube.com/watch?v=ZXD0lc83MpM",
+    summary: "This thing is incredible, check out the links below, you guys have to get one of these things! It's a great price, it's an astounding product - trust me.",
+  },
+  {
+    name: "Tech By Matt.",
+    comments: "Thanks for the shout out @TechByMatt! We're glad you're loving the #BuildRedux PC.",
+    img: ReviewAvatar8,
+    video: "https://cdn.shopify.com/s/files/1/0408/5792/7834/files/EmJIPphX0AMzzKG_800x.jpg?v=1615169785",
+    summary: "This is a system I've been testing from @BuildRedux. They're a new prebuilt company selling systems for the cost of the parts + a $75 build fee. They have amazing systems with Ryzen 5000 series CPUs and 3070s available for pre-order now. P.S. check out that cable management!",
+  },
+];
