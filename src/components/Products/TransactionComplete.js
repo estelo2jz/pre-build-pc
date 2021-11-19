69fd88a0d5c0a6aea789170f46635f1a3b6b566e
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-19 15:45:21 -0600

    Create TransactionComplete.js

---
+++ src/components/Products/TransactionComplete.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransactionComplete = () => {
+  return (
+    <div>
+      <h1>Thank you for your purchase!</h1>
+      <h1>Your order will be shipped out soon!</h1>
+      <h1>You will receive a confirmation Order #!</h1>
+    </div>
+  )
+}
+
+export default TransactionComplete
