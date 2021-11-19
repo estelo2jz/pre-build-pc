ac2535af89c479ce787a18c1839f87df3d02b265
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-19 15:45:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ import { DataProvider } from "./components/Products/Data/DataProvider";
 import Details from "./components/Products/Details/Details";
 
 import Checkout from "./components/Products/Chechout";
+import Shipping from "./components/Products/Shipping";
+import Payment from "./components/Products/Payment";
+import TransactionComplete from "./components/Products/TransactionComplete";
 
-import Login from './components/pages/Login/Login';
+import Login from "./components/pages/Login/Login";
 import About from "./components/pages/AboutUs/About";
 import BuildPC from "./components/pages/BuildPC/BuildPC";
 import Financing from "./components/pages/Financing/Financing";
@@ -28,25 +31,26 @@ function App() {
           <section>
             <Header />
             <Routes>
-              <ScrollToTop>
-                <Route path="/" exact element={<Home />}>
-                  <Home />
-                </Route>
+              <Route path="/" exact element={<Home />}>
+                <Home />
+              </Route>
 
-                <Route path="/products" exact element={<BuildPC />} />
-                <Route path="/pages/review" exact element={<Review />} />
-                <Route path="/pages/financing" exact element={<Financing />} />
-                <Route path="/pages/support" exact element={<Support />} />
-                <Route path="/pages/why-pre-build" exact element={<About />} />
-                <Route path="/checkout" exact element={<Checkout />} />
-                <Route path="/login" exact element={<Login />} />
+              <Route path="/products" exact element={<BuildPC />} />
+              <Route path="/pages/review" exact element={<Review />} />
+              <Route path="/pages/financing" exact element={<Financing />} />
+              <Route path="/pages/support" exact element={<Support />} />
+              <Route path="/pages/why-pre-build" exact element={<About />} />
+              <Route path="/checkout" exact element={<Checkout />} />
+              <Route path="/shipping" exact element={<Shipping />} />
+              <Route path="/payment" exact element={<Payment />} />
+              <Route path="/transaction" exact element={<TransactionComplete />} />
+              <Route path="/login" exact element={<Login />} />
 
-                {/* <Route path="/cart" exact render={() => <Redirect to="/cart" />} /> */}
-                {/* <Route path="/products" exact element={ <Products /> } /> */}
+              {/* <Route path="/cart" exact render={() => <Redirect to="/cart" />} /> */}
+              {/* <Route path="/products" exact element={ <Products /> } /> */}
 
-                <Route path="/products/:id" exact element={<Details />} />
-                <Route path="cart" exact element={<Cart />} />
-              </ScrollToTop>
+              <Route path="/products/:id" exact element={<Details />} />
+              <Route path="cart" exact element={<Cart />} />
             </Routes>
           </section>
           <Footer />
