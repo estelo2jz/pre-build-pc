a1373f0435fc205da4f7df6ce0299a881ec9aa3b
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-23 15:52:19 -0600

    Update ShippingForm.js

--- src/components/Products/ShippingForm.js
+++ src/components/Products/ShippingForm.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 // import PayPal from '../../assets/images/cart/paypay.svg';
 
-import "./styles/Shipping.scss";
+import "./styles/ShippingForm.scss";
 
 import { FcGoogle } from "react-icons/fc";
 import { BsPaypal } from "react-icons/bs";
@@ -40,19 +40,47 @@ const Shipping = () => {
             <p>Payment</p>
           </div>
         </div>
-        <div className="shipping_">
-          <div className="shipping__">
-            <div className="shipping__">
-              <p>Shipping Notice</p>
+        <div className="shipping__contact-container">
+          <div className="shipping__contact">
+            <div className="shipping__contact-top">
+              <p>Contact</p>
+              <input type="text" placeholder="Email" />
             </div>
-            <div className="shipping__">
+            <div className="shipping__contact-bottom">
+              <p>Ship to</p>
+              <input type="text" placeholder="Shipping Address" />
             </div>
           </div>
-          <div className="shipping__">
-            <div className="shipping__">
+        </div>
+        <div className="shipping__shipping-container">
+          <div className="shipping__notice">
+            <div className="shipping__notice-top">
+              <p>Shipping Notice</p>
+            </div>
+            <div className="shipping__notice-bottom">
+              <p>Your order will ship 10-14 days after processing.</p>
+            </div>
+          </div>
+          <div className="shipping__method-container">
+            <div className="shipping__method-heading">
               <p>Shipping method</p>
             </div>
-            <div className="shipping__">
+            <div className="shipping__method">
+              <div className="shipping__method-home">
+                <input type="radio" name="delivery" value="home" />
+                <p>FedEx Ground® Home Delivery</p>
+                <p>$45.00</p>
+              </div>
+              <div className="shipping__method-saver">
+                <input type="radio" name="delivery" value="saver" />
+                <p>FedEx Express Saver®</p>
+                <p>$97.00</p>
+              </div>
+              <div className="shipping__method-overnight">
+                <input type="radio" name="delivery" value="ovenight" />
+                <p>FedEx Standard Overnight®</p>
+                <p>$157.00</p>
+              </div>
             </div>
           </div>
         </div>
