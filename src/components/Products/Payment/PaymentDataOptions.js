a46e5151f0db7c7bf89f8742fdb8901dbcd3bb3c
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-23 18:33:04 -0600

    Create PaymentDataOptions.js

---
+++ src/components/Products/Payment/PaymentDataOptions.js
@@ -0,0 +1,17 @@
+export const Data = [
+  {
+    paymentOption: "Credit card",
+    answer:
+      "Click 'Build Your PC' at the top of our website to get started. Our online PC builder will guide you step-by-step on building the right gaming PC for your needs and budget.",
+  },
+  {
+    paymentOption: "Paypal",
+    answer:
+      "Once we process your order, we should have it shipped within 3-4 week. You will receive tracking information via email.",
+  },
+  {
+    paymentOption: "Pay over time",
+    answer:
+      "We offer a 30 day return policy from the day you receive your order, re-stocking fees may apply unless there is an issue with the PC.",
+  },
+];
