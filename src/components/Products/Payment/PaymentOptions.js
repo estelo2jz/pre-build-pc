b1d66c6ef32c6d745b07bcd1dcedd704c43cb1e9
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-23 18:32:22 -0600

    Create PaymentOptions.js

---
+++ src/components/Products/Payment/PaymentOptions.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AccordionSales from './PaymentData';
+
+import "./PaymentOptions.scss";
+
+const PaymentOptions = () => {
+  return (
+    <div className="payment__container">
+      <AccordionSales />
+    </div>
+  );
+};
+
+export default PaymentOptions;
