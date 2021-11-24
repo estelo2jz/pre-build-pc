be21b866c087d0206cbc58c63a1685edd994ead3
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-11-23 19:29:21 -0600

    Update PaymentData.js

--- src/components/Products/Payment/PaymentData.js
+++ src/components/Products/Payment/PaymentData.js
@@ -11,7 +11,7 @@ const AccordionSection = styled.div`
   justify-content: center;
   position: relative;
   height: 100%;
-  margin-top: 20px;
+  /* margin-top: 20px; */
   background: transparent;
   `;
 
@@ -20,12 +20,13 @@ const Container = styled.div`
   background: #f0f0f0;
   position: relative;
   top: 30%;
-  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+  border: 1px solid #d9d9d9;
+  /* box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3); */
   
 `;
 
 const Wrap = styled.div`
-  border-bottom: 1px solid rgba(194, 91, 233, 0.2);
+  /* border-bottom: 1px solid #d9d9d9; */
   background-color: #fff;
   color: #333;
   display: flex;
@@ -49,18 +50,18 @@ const Wrap = styled.div`
   `;
 
 const Dropdown = styled.div`
-  background-color: #fff;
+  background-color: #fafafa;
+  border-bottom: 1px solid #d9d9d9;
   color: #333;
   width: 100%;
-  height: 100px;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  p {
+  padding: 10px;
+  /* p {
     font-size: clamp(10px, 2.5vw, 15px);
     padding: 0px 20px;
-  }
+  } */
 `;
 
 const Accordion = () => {
