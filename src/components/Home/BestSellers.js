3b6f6fde8b7aea7c72dcc21ff6584e2675dfa8a8
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-30 21:47:33 -0600

    Update BestSellers.js

--- src/components/Home/BestSellers.js
+++ src/components/Home/BestSellers.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import Products from '../Products/Products';
+import React from "react";
+import { Parallax } from "react-parallax";
+
+import Products from "../Products/Products";
+import FortniteBG from "./fortnite.jpg";
+import "./styles/parallax.scss";
 
 const BestSellers = () => {
   return (
+    // <Parallax
+    //   className="image"
+    //   blur={0}
+    //   bgImage={FortniteBG}
+    //   strength={800}
+    //   bgImageStyle={{ minheight: "100%" }}
+    // >
+    // </Parallax>
     <div>
-      <Products />
+      <div className="content">
+        <Products />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSellers
+export default BestSellers;
