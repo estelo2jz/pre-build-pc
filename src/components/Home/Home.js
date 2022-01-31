62bdfd34f5f864bc2d293943830b8b02112a5c8a
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-30 21:47:25 -0600

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import TopHeader from './TopHeader';
-import Pricing from './Pricing';
-import Review from './Review';
-import BestSellers from './BestSellers';
-import Follow from './Follow';
+import React from "react";
+import { Parallax } from 'react-parallax';
+
+import TopHeader from "./TopHeader";
+import Pricing from "./Pricing";
+import Review from "./Review";
+import BestSellers from "./BestSellers";
+import Follow from "./Follow";
+import FooterOurMission from "../Footer/FooterOurMission";
+import Checkout from "../Footer/CheckoutBuild";
+
+import "./styles/Home.scss";
 
 const Home = () => {
   return (
-    <div>
-      {/* top header with BG */}
-      <TopHeader />
-      <Pricing />
-      <Review />
-      <BestSellers />
-      <Follow />
-      {/* best sellers component*/}
-      {/* follow component */}
-      {/* from  */}
-    </div>
-  )
-}
+    <div className="home__main">
+      <div className="home__main__top-header">
+        <TopHeader />
+      </div>
+      <div className="home__main__price">
+        <Pricing />
+      </div>
 
-export default Home
+      <div className="home__main__review">
+        <Review />
+      </div>
+      <div className="home__main__best-sellers">
+        <BestSellers />
+      </div>
+
+      <div className="home__main__follow">
+        <Follow />
+      </div>
+
+      <div className="home__main__checkout">
+        <Checkout />
+      </div>
+
+      <div className="home__main__footer">
+        <FooterOurMission />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
