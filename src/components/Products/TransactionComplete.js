2d9ea619a88b7cf7baa5a77de71b29e23a07ba36
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-30 21:47:20 -0600

    Update TransactionComplete.js

--- src/components/Products/TransactionComplete.js
+++ src/components/Products/TransactionComplete.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './styles/Transaction.scss';
 
 const TransactionComplete = () => {
   return (
-    <div>
+    <div className="transaction__main">
       <h1>Thank you for your purchase!</h1>
       <h1>Your order will be shipped out soon!</h1>
       <h1>You will receive a confirmation Order #!</h1>
