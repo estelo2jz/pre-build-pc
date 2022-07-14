9ddd55120f5aa198800fc9935a47325c0591b545
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-14 10:19:19 -0500

    Update ParallaxThree.js

--- src/components/Parallax/ParallaxThree.js
+++ src/components/Parallax/ParallaxThree.js
@@ -8,7 +8,6 @@ const ParallaxThree = () => {
   return (
     <Parallax className="image" blur={0} bgImage={WarzoneBG} strength={800} bgImageStyle={{minheight: "100vh"}}>
       <div className="content">
-        <span className="img-txt">a trip to Sapce</span>
       </div>
     </Parallax>
   );
