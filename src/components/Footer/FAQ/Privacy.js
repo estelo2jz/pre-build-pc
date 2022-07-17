2790f5e392de5362e70328202361c92ef6ab3d73
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-17 13:18:01 -0500

    Create Privacy.js

---
+++ src/components/Footer/FAQ/Privacy.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles/privacy.scss';
+
+export default function Privacy() {
+  return (
+    <div className="privacy__outer-container">
+      <div className="privacy__outer-inner">Currently Buiilding This Page.</div>
+    </div>
+  );
+}
