c5de65864aba138d900c90c7e5de5da2a5db57a4
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-17 10:41:20 -0500

    Create TC.js

---
+++ src/components/Footer/FAQ/TC.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import ReactLoading from 'react-loading';
+import './tc.css';
+
+export default class TC extends Component {
+  render() {
+    return (
+      <div>
+        <div className="tc__outer-container">I'm the Terms & Conditions</div>
+      </div>
+    )
+  }
+}
+
+
+// import React, { useState, useEffect } from 'react'
+// import LoadingScreen from './loading'
+
+
+// function TC() {
+//   const [loading, setLoading] = useState(true)
+
+//   useEffect(() => {
+//     setTimeout(() => setLoading(false), 1500)
+//   }, [])
+
+//   return (
+//     <>
+//     {loading === false ? (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//       ) : (
+//         <LoadingScreen />
+//       )}
+//       </>
+//   );
+// }
+
+// export default TC;
