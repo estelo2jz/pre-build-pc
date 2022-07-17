0409e42a3fc21e1cf56d849a3734eaacafdd9687
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-17 17:31:54 -0500

    Update Modal.js

--- src/components/Products/components/Modal/Modal.js
+++ src/components/Products/components/Modal/Modal.js
@@ -1,7 +1,9 @@
-
 import React from "react";
-import styles from "../Modal/Modal.module.css";
+import {Link} from "react-router-dom";
+import styles from "../Modal/Modal.module.scss";
 import { RiCloseLine } from "react-icons/ri";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import './modalll.scss';
 
 const Modal = ({ setIsOpen }) => {
   return (
@@ -15,20 +17,22 @@ const Modal = ({ setIsOpen }) => {
           {/* <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
             <RiCloseLine style={{ marginBottom: "-3px" }} />
           </button> */}
-          <div className={styles.modalContent}>
-            Item added to cart!
-          </div>
+          <div className={styles.modalContent}>Item added to cart!</div>
           <div className={styles.modalActions}>
             <div className={styles.actionsContainer}>
-              <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
-                Close
-              </button>
-              {/* <button
-                className={styles.cancelBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-              </button> */}
+              <Link  to="/cart"> 
+                <div className="go-to-cart__container">
+                  <button className="cancelBtn" onClick={() => setIsOpen(false)}>
+                    <AiOutlineShoppingCart />
+                  </button>
+                </div>
+              </Link> 
+              <div className="close__container">
+                <button className="delete__Btn" onClick={() => setIsOpen(false)}>
+                  Close
+                </button>
+              </div>
+                <AiOutlineShoppingCart />
             </div>
           </div>
         </div>
