9b91f1e77005220b5a26f93ade36f90487bf6185
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-17 13:17:58 -0500

    Create Refund.js

---
+++ src/components/Footer/FAQ/Refund.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles/refund.scss';
+
+export default function Refund() {
+  return (
+    <div className="refund__outer-container">
+      <div className="refund__outer-inner">Currently Buiilding This Page.</div>
+    </div>
+  );
+}
